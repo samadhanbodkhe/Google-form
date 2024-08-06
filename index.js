4479596d6973bb98f1bc35dc4924066d28530930
unknown <bodkhesamadhan188@gmail.com>
2024-08-06 15:02:23 +0530

    parse err

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("dist"))
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: "*", credentials: true }))
+app.use(cors({ origin: "https://google-form-u5pq.onrender.com", credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/google-auth", require("./routes/authRoute"))
